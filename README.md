724fbec212821a5d8c0255e408666fcc830d3712
Jason More <Jason.More@gmail.com>
2024-05-17 09:03:57 -0500

    prettier format

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Notion: https://www.notion.so/streak/Jason-More-f83e1e5f08e04008ab7503da516ef03c
 
 Requirements:
-* Search using text and chips
-* Keyboard navigation
+
+- Search using text and chips
+- Keyboard navigation
---
+++ src/searchbar/Searchbar.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export const Searchbar = () => {
+  //   const [currentInput, setCurrentInput] = useState('');
+  //   const [tokens, setTokens] = useState([]);
+  //
+  //   const handleInputChange = (event) => {
+  //     setCurrentInput(event.target.value);
+  //   };
+  //
+  //   const tokenizeInput = () => {
+  //     if (currentInput.trim()) {
+  //       setTokens([...tokens, currentInput.trim()]);
+  //       setCurrentInput('');
+  //     }
+  //   };
+  //
+  //   const handleKeyDown = (event) => {
+  //     if (event.key === 'Enter' || event.key === ',') {
+  //       event.preventDefault();
+  //       tokenizeInput();
+  //     }
+  //   };
+  //
+  //   return (
+  //     <div className="container">
+  //     <input
+  //       type="text"
+  //       value={currentInput}
+  //       onChange={handleInputChange}
+  //       onKeyDown={handleKeyDown}
+  //       placeholder="Type and press enter..."
+  //       autoComplete="off"
+  //       aria-label="Enter tokens"
+  //     />
+  //   <div>
+  //     {tokens.map((token, index) => (
+  //       <span key={index} className="token">{token}</span>
+  //     ))}
+  //   </div>
+  //     </div>
+  // )
+};
