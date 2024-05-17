81b11d4d69c9d3b77aa9a2eb9528a436df2783c5
Jason More <Jason.More@gmail.com>
2024-05-17 09:47:54 -0500

    ui formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Searchbar } from "./searchbar/Searchbar.tsx";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <Searchbar />
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app-container">
+      <h1>Searchbar Chip</h1>
+      <Searchbar />
+    </div>
   );
 }
 
--- src/searchbar/Searchbar.tsx
+++ src/searchbar/Searchbar.tsx
@@ -1,43 +1,54 @@
 import { useState } from "react";
+import viteLogo from "/vite.svg";
+import searchIcon from "/search.svg";
+import "./Searchbar.css";
 
 export const Searchbar = () => {
-  //   const [currentInput, setCurrentInput] = useState('');
-  //   const [tokens, setTokens] = useState([]);
-  //
-  //   const handleInputChange = (event) => {
-  //     setCurrentInput(event.target.value);
-  //   };
-  //
-  //   const tokenizeInput = () => {
-  //     if (currentInput.trim()) {
-  //       setTokens([...tokens, currentInput.trim()]);
-  //       setCurrentInput('');
-  //     }
-  //   };
-  //
-  //   const handleKeyDown = (event) => {
-  //     if (event.key === 'Enter' || event.key === ',') {
-  //       event.preventDefault();
-  //       tokenizeInput();
-  //     }
-  //   };
-  //
-  //   return (
-  //     <div className="container">
-  //     <input
-  //       type="text"
-  //       value={currentInput}
-  //       onChange={handleInputChange}
-  //       onKeyDown={handleKeyDown}
-  //       placeholder="Type and press enter..."
-  //       autoComplete="off"
-  //       aria-label="Enter tokens"
-  //     />
-  //   <div>
-  //     {tokens.map((token, index) => (
-  //       <span key={index} className="token">{token}</span>
-  //     ))}
-  //   </div>
-  //     </div>
-  // )
+  const [currentInput, setCurrentInput] = useState("");
+  const [tokens, setTokens] = useState([]);
+
+  const handleInputChange = (event: Event) => {
+    setCurrentInput(event.target.value);
+  };
+
+  const tokenizeInput = () => {
+    if (currentInput.trim()) {
+      setTokens([...tokens, currentInput.trim()]);
+      setCurrentInput("");
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter" || event.key === ",") {
+      event.preventDefault();
+      tokenizeInput();
+    }
+  };
+
+  return (
+    <div className="searchbar-container">
+      <img src={viteLogo} className="vite-logo" alt="Vite logo" />
+
+      <div className="search-box">
+        <img src={searchIcon} className="search-icon" alt="Vite logo" />
+
+        <input
+          className="search-input"
+          type="text"
+          value={currentInput}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+          placeholder="Filter"
+          autoComplete="off"
+        />
+      </div>
+      <div>
+        {tokens.map((token, index) => (
+          <span key={index} className="token">
+            {token}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 };
