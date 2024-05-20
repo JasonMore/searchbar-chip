78a9e89ce2e42eabf24b101a2fd025b9c0791dbd
Jason More <Jason.More@gmail.com>
2024-05-20 09:01:46 -0500

    refactor

--- src/searchbar/tokenize.ts
+++ src/searchbar/tokenize.ts
@@ -7,28 +7,28 @@ const operatorMap = {
   "": "equals",
 };
 
-export const tokenize = (textContent: string, fieldType: FieldType): Token => {
-  const match = /(?<field>\w+):(?<operator>[-<>]?)(?<value>\w+)/gi.exec(
+export const parseTextContent = (textContent: string): Partial<Token> => {
+  const match = /(?<field>\w+)?:(?<operator>[-<>]?)?(?<value>\w+)?/gi.exec(
     textContent,
   );
-  if (!match) {
-    return {
-      text: textContent,
-      field: "",
-      type: fieldType,
-      operator: "unknown",
-      value: "",
-    };
-  }
-
-  // TODO: check type with type predicate https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates
-  const operator = operatorMap[match.groups?.operator ?? ""];
 
   return {
     text: textContent,
-    field: match.groups?.field ?? "--missing field--",
+    field: match?.groups?.field,
+    // TODO: check type with type predicate https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates
+    operator: operatorMap[match?.groups?.operator ?? ""] ?? "unknown",
+    value: match?.groups?.value,
+  };
+};
+
+export const tokenize = (textContent: string, fieldType: FieldType): Token => {
+  const parsed = parseTextContent(textContent)
+
+  return {
+    text: textContent,
+    field: parsed.field ?? "--missing field--",
     type: fieldType,
-    operator,
-    value: match.groups?.value ?? "--missing value",
+    operator: parsed.operator,
+    value: parsed.value ?? "--missing value--",
   };
 };
