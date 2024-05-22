7ee06dce42873a301b495af7451343af75ba0f37
Jason More <Jason.More@gmail.com>
2024-05-22 12:25:31 -0500

    ts error went away

--- src/searchbar/Chip.tsx
+++ src/searchbar/Chip.tsx
@@ -1,19 +1,19 @@
 import "./Chip.css";
 import type { Token } from "./types.ts";
-import { forwardRef, KeyboardEvent } from "react";
+import { forwardRef, KeyboardEvent, RefObject } from "react";
 
 type Props = {
   token: Token;
   removeToken: (token: Token) => void;
-  prevChipRef: React.RefObject<HTMLInputElement>;
-  nextChipRef: React.RefObject<HTMLInputElement>;
+  prevChipRef: RefObject<HTMLInputElement>;
+  nextChipRef: RefObject<HTMLInputElement>;
 };
 
 export const Chip = forwardRef<HTMLInputElement, Props>(
   ({ token, removeToken, prevChipRef, nextChipRef }, ref) => {
-    const valid = token.operator != "unknown";
+    const valid = token.operator !== "unknown";
 
-    const onKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+    const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
       const isAtStart = e.currentTarget.selectionStart === 0;
       const isAtEnd =
         e.currentTarget.selectionStart === e.currentTarget.value.length;
@@ -21,17 +21,16 @@ export const Chip = forwardRef<HTMLInputElement, Props>(
       if (e.key === "ArrowLeft" && isAtStart) {
         e.preventDefault();
         if (prevChipRef?.current) {
-          prevChipRef?.current.focus();
-          prevChipRef?.current?.selectionStart =
-            prevChipRef?.current.value.length;
+          prevChipRef.current.focus();
+          prevChipRef.current.selectionStart = prevChipRef.current.value.length;
         }
       }
 
       if (e.key === "ArrowRight" && isAtEnd) {
         e.preventDefault();
         if (nextChipRef?.current) {
-          nextChipRef?.current.focus();
-          nextChipRef?.current.selectionStart = 0;
+          nextChipRef.current.focus();
+          nextChipRef.current.selectionStart = 0;
         }
       }
     };
@@ -40,7 +39,7 @@ export const Chip = forwardRef<HTMLInputElement, Props>(
       <div className={`chip ${valid ? "chip-valid" : ""}`}>
         <input
           ref={ref}
-          onKeyUp={onKeyUp}
+          onKeyDown={onKeyDown}
           className="chip-text"
           value={token.text}
         />
@@ -49,5 +48,5 @@ export const Chip = forwardRef<HTMLInputElement, Props>(
         </span>
       </div>
     );
-  },
+  }
 );
